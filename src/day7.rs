8080208813dd0b5d2ab4384bf170a3d20c93a706
Andrea Ambu <andreambu@gmail.com>
2024-12-08 09:38:06 +0000

    day7 p1 770us p2 15ms sooo slow

--- src/day7.rs
+++ src/day7.rs
@@ -1,6 +1,6 @@
 use aoc_runner_derive::aoc;
 
-fn solve(line: &str, part2: bool) -> u64 {
+fn solve_dfs(line: &str, part2: bool) -> u64 {
     if let Some((sgoal, sterms)) = line.split_once(':') {
         let goal = sgoal
             .as_bytes()
@@ -8,9 +8,22 @@ fn solve(line: &str, part2: bool) -> u64 {
             .fold(0u64, |acc, b| acc * 10 + (b - b'0') as u64);
         let terms: Vec<u64> = sterms
             .split_ascii_whitespace()
-            .map(|t| t.parse::<u64>().unwrap())
+            .map(|t| {
+                t.as_bytes()
+                    .iter()
+                    .fold(0u64, |acc, b| acc * 10 + (b - b'0') as u64)
+            })
             .collect();
-        let mut fringe = Vec::with_capacity(32);
+        let termslog10 = if part2 {
+            terms
+                .iter()
+                .map(|&t| (t as f64).log10().floor() as u32)
+                .map(|log10| 10u64.pow(log10 + 1))
+                .collect()
+        } else {
+            Vec::new()
+        };
+        let mut fringe = Vec::with_capacity(12);
         fringe.push((1usize, terms[0]));
         while let Some((idx, partial)) = fringe.pop() {
             if partial > goal {
@@ -22,12 +35,11 @@ fn solve(line: &str, part2: bool) -> u64 {
                 }
                 continue;
             }
-            let term = terms[idx];
+            let term = unsafe { terms.get_unchecked(idx) };
             fringe.push((idx + 1, partial * term));
             fringe.push((idx + 1, partial + term));
             if part2 {
-                let log10 = (term as f64).log10().floor() as u32;
-                let mul = 10u64.pow(log10 + 1);
+                let mul = unsafe { termslog10.get_unchecked(idx) };
                 fringe.push((idx + 1, partial * mul + term));
             }
         }
@@ -36,18 +48,82 @@ fn solve(line: &str, part2: bool) -> u64 {
     0
 }
 
-#[aoc(day7, part1)]
-pub fn part1(input: &str) -> u64 {
-    input.lines().map(|line| solve(line, false)).sum()
+fn recurse(goal: u64, partial: u64, sterms: &str, part2: bool) -> u64 {
+    if partial > goal {
+        return 0;
+    }
+    if sterms.is_empty() {
+        return if partial == goal { goal } else { 0 };
+    }
+    let (sterm, rest) = match sterms.split_once(' ') {
+        Some((term, rest)) => (term, rest),
+        None => (sterms, ""),
+    };
+    let term = sterm
+        .as_bytes()
+        .iter()
+        .fold(0u64, |acc, b| acc * 10 + (b - b'0') as u64);
+    let times = recurse(goal, partial * term, rest, part2);
+    if times > 0 {
+        return times;
+    }
+    let plus = recurse(goal, partial + term, rest, part2);
+    if plus > 0 {
+        return plus;
+    }
+    if part2 {
+        let term = sterm
+            .as_bytes()
+            .iter()
+            .fold(partial, |acc, b| acc * 10 + (b - b'0') as u64);
+        return recurse(goal, term, rest, part2);
+    }
+    0
 }
 
-#[aoc(day7, part2)]
+fn solve_recurse(line: &str, part2: bool) -> u64 {
+    if let Some((sgoal, sterms)) = line.split_once(':') {
+        let goal = sgoal
+            .as_bytes()
+            .iter()
+            .fold(0u64, |acc, b| acc * 10 + (b - b'0') as u64);
+        return recurse(goal, 0, sterms, part2);
+    }
+    0
+}
+
+#[aoc(day7, part1, base)]
+pub fn part1(input: &str) -> u64 {
+    input.lines().map(|line| solve_dfs(line, false)).sum()
+}
+
+#[aoc(day7, part2, base)]
 pub fn part2(input: &str) -> u64 {
     input
         .lines()
         .map(|line| {
-            let s1 = solve(line, false);
-            return if s1 == 0 { solve(line, true) } else { s1 };
+            let s1 = solve_dfs(line, false);
+            return if s1 == 0 { solve_dfs(line, true) } else { s1 };
+        })
+        .sum()
+}
+
+#[aoc(day7, part1, recurse)]
+pub fn part1_recurse(input: &str) -> u64 {
+    input.lines().map(|line| solve_recurse(line, false)).sum()
+}
+
+#[aoc(day7, part2, recurse)]
+pub fn part2_recurse(input: &str) -> u64 {
+    input
+        .lines()
+        .map(|line| {
+            let s1 = solve_recurse(line, false);
+            return if s1 == 0 {
+                solve_recurse(line, true)
+            } else {
+                s1
+            };
         })
         .sum()
 }
@@ -60,10 +136,12 @@ mod tests {
     #[test]
     fn part1_example() {
         assert_eq!(part1(INPUT), 538191549061);
+        assert_eq!(part1_recurse(INPUT), 538191549061);
     }
 
     #[test]
     fn part2_example() {
         assert_eq!(part2(INPUT), 34612812972206);
+        assert_eq!(part2_recurse(INPUT), 34612812972206);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-mod day7;
 extern crate aoc_runner;
 
 #[macro_use]
@@ -7,6 +6,6 @@ extern crate aoc_runner_derive;
 pub mod day1;
 pub mod day2;
 pub mod day3;
-pub mod day6;
+pub mod day7;
 
 aoc_lib!(year = 2024);
