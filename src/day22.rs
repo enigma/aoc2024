4650c99206e70ea089a0ea9d5c44bb956b7b0fa6
Andrea Ambu <andreambu@gmail.com>
2024-12-22 18:13:55 +0000

    bit trickery

--- src/day22.rs
+++ src/day22.rs
@@ -36,6 +36,7 @@ pub fn part1(input: &str) -> u64 {
 pub fn part2base(input: &str) -> u64 {
     let mut secrets = parse(input);
     let mut counter: FxHashMap<_, _> = FxHashMap::default();
+    // let mut counter = [0; CACHE_SIZE];
     let mut result = 0;
     let mut caches: Vec<FxHashMap<u64, u64>> =
         vec![FxHashMap::with_capacity_and_hasher(1 << 10, Default::default()); secrets.len()];
@@ -77,20 +78,22 @@ const CACHE_SIZE: usize = 19 * 19 * 19 * 19 + 7;
 pub fn part2inv(input: &str) -> u64 {
     let mut secrets = parse(input);
     let mut counter = [0; CACHE_SIZE];
-    let mut seen = [false; CACHE_SIZE];
+    let mut seen = [0u64; (CACHE_SIZE + 63) / 64];
     let mut result = 0;
     secrets.iter_mut().for_each(|secret| {
-        seen.fill(false);
+        seen.fill(0);
         let (mut a, mut b, mut c, mut d) = (0, 0, 0, 0);
         for i in 0..2000 {
             let was = *secret;
             *secret = next_secret(*secret);
             let delta = 9 + (*secret % 10) - (was % 10);
             (a, b, c, d) = (b * 19, c * 19, d * 19, delta);
-            let seen_hash = (a + b + c + d) as usize;
             if i >= 4 {
-                if !seen[seen_hash] {
-                    seen[seen_hash] = true;
+                let seen_hash = (a + b + c + d) as usize;
+                let idx = seen_hash / 64;
+                let bit = seen_hash % 64;
+                if seen[idx] & (1 << bit) == 0 {
+                    seen[idx] |= 1 << bit;
                     counter[seen_hash] += *secret % 10;
                     result = result.max(counter[seen_hash]);
                 }
